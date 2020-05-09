b6ef533f299da007596c18484df615ead2ddc06b
telefunk <telewebb@gmail.com>
2020-05-09 14:57:50 -0500

    Completed Module 5

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -8,6 +8,15 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public Customer()
+        {
+
+        }
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+        }
+
         public int CustomerId { get; private set; }
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
@@ -29,5 +38,50 @@ namespace ACM.BL
             }
         }
         public static int InstanceCount { get; set; }
+
+        /// <summary>
+        /// Retrieve one customer.
+        /// </summary>
+        public Customer Retrieve(int customerId)
+        {
+            // Code that retrieves the defined customer
+
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Retrieve all customers.
+        /// </summary>
+        public List<Customer> Retrieve()
+        {
+            // Code that retrieves all of the customers
+
+            return new List<Customer>();
+        }
+
+        /// <summary>
+        /// Saves the current customer.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined customer
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
     }
 }
